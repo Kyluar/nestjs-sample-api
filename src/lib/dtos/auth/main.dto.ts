cdb5a0269ed6899ca4c8ae1e1f8e182f1e03508c
Gabriel Ribeiro Ramos <104779676+Kyluar@users.noreply.github.com>
2025-08-08 08:34:30 -0300

    Fixing Const Name

--- src/lib/dtos/auth/main.dto.ts
+++ src/lib/dtos/auth/main.dto.ts
@@ -1,9 +1,9 @@
 import { z } from '@/lib/config/zod'
 import { createZodDto } from 'nestjs-zod'
 
-export const loginShema = z.strictObject({
+export const loginSchema = z.strictObject({
   email: z.email(),
   password: z.string().min(10),
 })
 
-export class LoginDto extends createZodDto(loginShema) {}
+export class LoginDto extends createZodDto(loginSchema) {}
