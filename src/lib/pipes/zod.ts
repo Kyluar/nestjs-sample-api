0d11183e7ca0e77d1434c66c149d7a678ff61752
Gabriel Ribeiro Ramos <104779676+Kyluar@users.noreply.github.com>
2025-11-28 09:31:37 -0300

    :bug: fix: Fixing Zod Pipe bug

--- src/lib/pipes/zod.ts
+++ src/lib/pipes/zod.ts
@@ -1,10 +1,11 @@
 import { ZodError } from 'zod'
-import { createZodValidationPipe } from 'nestjs-zod'
+import { createZodValidationPipe, ZodValidationPipe } from 'nestjs-zod'
 import { BadRequestException } from '@nestjs/common'
 
-export const CustomZodValidationPipe = createZodValidationPipe({
-  createValidationException: (error: ZodError) =>
-    new BadRequestException({
-      message: error.issues[0].message,
-    }),
-})
+export const CustomZodValidationPipe: typeof ZodValidationPipe =
+  createZodValidationPipe({
+    createValidationException: (error: ZodError) =>
+      new BadRequestException({
+        message: error.issues[0].message,
+      }),
+  })
